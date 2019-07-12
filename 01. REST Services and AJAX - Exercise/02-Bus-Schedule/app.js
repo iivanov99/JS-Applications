7c5471917216080269f3a04a48f0ce7e421af3cf
iivanov99 <ivko3run@gmail.com>
2019-07-12 23:24:13 +0300

    Exercise 02-Bus-Schedule modified

--- 01. REST Services and AJAX - Exercise/02-Bus-Schedule/app.js
+++ 01. REST Services and AJAX - Exercise/02-Bus-Schedule/app.js
@@ -1,17 +1,56 @@
-function solve() {
+const SELECTORS = {
+    infoSpan: '.info',
+    departBtn: '#depart',
+    arriveBtn: '#arrive'
+};
 
-    function depart() {
-        console.log('Depart TODO...');
-    }
+const currentStop = {
+    name: '',
+    nextId: 'depot'
+};
 
-    function arrive() {
-        console.log('Arrive TODO...');
-    }
+const getInfoSpan = () => document.querySelector(SELECTORS.infoSpan);
+
+const getDepartButton = () => document.querySelector(SELECTORS.departBtn);
+
+const getArriveButton = () => document.querySelector(SELECTORS.arriveBtn);
+
+const result = (function () {
+    const infoSpan = getInfoSpan();
+    const departBtn = getDepartButton();
+    const arriveBtn = getArriveButton();
+
+    const depart = function () {
+        url = `https://judgetests.firebaseio.com/schedule/${currentStop.nextId}.json`;
+
+        fetch(url)
+            .then(response => {
+                if (response.status !== 200) {
+                    infoSpan.textContent = 'Error';
+                    departBtn.disabled = true;
+                    arriveBtn.disabled = true;
+                    return;
+                }
+                return response.json()
+            })
+            .then(stopInfo => {
+                currentStop.name = stopInfo.name;
+                currentStop.nextId = stopInfo.next;
+
+                infoSpan.textContent = `Next stop ${currentStop.name}`;
+                departBtn.disabled = true;
+                arriveBtn.disabled = false;
+            });
+    };
+
+    const arrive = function () {
+        infoSpan.textContent = `Arriving at ${currentStop.name}`;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+    };
 
     return {
         depart,
         arrive
     };
-}
-
-let result = solve();
+})();
