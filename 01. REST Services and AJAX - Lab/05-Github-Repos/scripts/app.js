a401052e544da4553edfc87f06033e603870c7c8
iivanov99 <ivko3run@gmail.com>
2019-07-12 14:19:38 +0300

    05-Github-Repos finished

--- 01. REST Services and AJAX - Lab/05-Github-Repos/scripts/app.js
+++ 01. REST Services and AJAX - Lab/05-Github-Repos/scripts/app.js
@@ -1,3 +1,30 @@
+const getUsername = () => document.querySelector('#username').value;
+const getReposUl = () => document.querySelector('#repos');
+
 function loadRepos() {
-	console.log("TODO...");
+    const username = getUsername();
+    const reposUl = getReposUl();
+    reposUl.innerHTML = '';
+
+    const appendRepo = (repo) => {
+        const li = document.createElement('li');
+
+        const a = document.createElement('a');
+        a.href = repo.html_url;
+        a.textContent = repo.full_name;
+
+        li.appendChild(a);
+        reposUl.appendChild(li);
+    }
+
+    const appendError = (err) => {
+        const li = document.createElement('li');
+        li.textContent = err;
+        reposUl.appendChild(li);
+    }
+
+    fetch(`https://api.github.com/users/${username}/repos`)
+        .then(response => response.json())
+        .then((reposData) => reposData.forEach(repoData => appendRepo(repoData)))
+        .catch(err => appendError(err));
 }
