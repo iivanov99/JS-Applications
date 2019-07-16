01542924bda5f6741e4dded70b8e92257c8ede86
iivanov99 <ivko3run@gmail.com>
2019-07-16 10:15:38 +0300

    02-Blog finished

--- 02. Asynchronous Programming - Lab/02-Blog/app.js
+++ 02. Asynchronous Programming - Lab/02-Blog/app.js
@@ -1,5 +1,85 @@
-function attachEvents() {
-    console.log('TODO...');
-}
+const SELECTORS = {
+    loadPostsBtn: '#btnLoadPosts',
+    postsSelect: '#posts',
+    viewPostBtn: '#btnViewPost',
+    postTitle: '#post-title',
+    postBodyUl: '#post-body',
+    postCommentsUl: '#post-comments'
+};
 
-attachEvents();
+const getLoadPostsButton = () => document.querySelector(SELECTORS.loadPostsBtn);
+
+const getPostsSelect = () => document.querySelector(SELECTORS.postsSelect);
+
+const getViewPostButton = () => document.querySelector(SELECTORS.viewPostBtn);
+
+const getPostTile = () => document.querySelector(SELECTORS.postTitle);
+
+const getPostBodyUl = () => document.querySelector(SELECTORS.postBodyUl);
+
+const getPostCommentsUl = () => document.querySelector(SELECTORS.postCommentsUl);
+
+const createOptions = (selectElement, posts) => {
+    for (const id in posts) {
+        const option = document.createElement('option');
+        option.value = id;
+        option.textContent = posts[id].title;
+        selectElement.appendChild(option);
+    }
+};
+
+const loadPosts = () => {
+    const postsSelect = getPostsSelect();
+    const url = 'https://blog-apps-c12bf.firebaseio.com/posts.json';
+    postsSelect.innerHTML = '';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(posts => createOptions(postsSelect, posts));
+};
+
+const findCommentByIdAndDisplayIt = (ulElement, comments, id) => {
+    for (const key in comments) {
+        if (comments[key].postId === id) {
+            const li = document.createElement('li');
+            li.textContent = comments[key].text;
+            ulElement.appendChild(li);
+        }
+    }
+};
+
+const displayPostComments = (post) => {
+    const postCommentsUl = getPostCommentsUl();
+    const url = `https://blog-apps-c12bf.firebaseio.com/comments.json`;
+    postCommentsUl.innerHTML = '';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(comments => findCommentByIdAndDisplayIt(postCommentsUl, comments, post.id));
+};
+
+const displayPostInfo = (post) => {
+    const postTitle = getPostTile();
+    const postBodyUl = getPostBodyUl();
+    postTitle.textContent = post.title;
+    postBodyUl.textContent = post.body;
+};
+
+const viewSelectedPost = () => {
+    const id = getPostsSelect().value;
+    const url = `https://blog-apps-c12bf.firebaseio.com/posts/${id}.json`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(post => {
+            displayPostInfo(post);
+            displayPostComments(post);
+        });
+};
+
+(function attachEvents() {
+    const loadPostsBtn = getLoadPostsButton();
+    const viewPostBtn = getViewPostButton();
+    loadPostsBtn.addEventListener('click', loadPosts);
+    viewPostBtn.addEventListener('click', viewSelectedPost);
+})();
